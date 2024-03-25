de70b1017b2e661a049dbec7848e73b0a8a27414
Pedro Alexandre Simões Lorenzutti <pedro.lorenzutti@gmail.com>
2024-03-25 20:43:18 -0300

    Adicionado rev_05.c mais organizado

--- 01_revisao/rev_05.c
+++ 01_revisao/rev_05.c
@@ -1,38 +1,69 @@
 #include <stdio.h>
 
-#define MAX_LIN 1000
-#define MAX_COL 1000
-
-typedef struct {
+typedef struct
+{
     int linha, coluna;
-} tipoPosicao;
+}
+tipoPosicao;
 
-int main() {
-    int numeroLinhas, numeroColunas;
-    int i, j;
-    tipoPosicao inicial, final, atual, proximo;
-
-    scanf("%d %d", &numeroLinhas, &numeroColunas);
-
-    int matriz[MAX_LIN][MAX_COL];
-
-    // Pegando a matriz
-    for (i = 0; i < numeroLinhas; i++) {
-        for (j = 0; j < numeroColunas; j++) {
+void lerMatriz(int numeroLinhas, int numeroColunas, int matriz[numeroLinhas][numeroColunas])
+{
+    for(int i = 0; i < numeroLinhas; i++){
+        for(int j = 0; j < numeroColunas; j++){
             scanf("%d", &matriz[i][j]);
         }
     }
+}
+
+void printarMatriz(int numeroLinhas, int numeroColunas, int matriz[numeroLinhas][numeroColunas])
+{
+    for(int i = 0; i < numeroLinhas; i++){
+        for(int j = 0; j < numeroColunas; j++){
+            printf("%d ", matriz[i][j]);
+        }
+
+        printf("\n");
+    }
+}
+
+void printarPosicao(tipoPosicao atual)
+{
+    printf("(%d, %d) ", atual.linha + 1, atual.linha + 1);
+}
+
+tipoPosicao removerUm(tipoPosicao posicao)
+{
+    posicao.linha--;
+    posicao.coluna--;
+    
+    return posicao;
+}
+
+int main() {
+    int numeroLinhas, numeroColunas;
+    tipoPosicao inicial, final, atual, proximo;
+
+    //Pegando o número de linhas e colunas
+    scanf("%d %d", &numeroLinhas, &numeroColunas);
+
+    int matriz[numeroLinhas][numeroColunas];
+
+    //Pegando a matriz
+    lerMatriz(numeroLinhas, numeroColunas, matriz);
 
     // Pegando a posição inicial e final
     scanf("%d %d", &inicial.linha, &inicial.coluna);
     scanf("%d %d", &final.linha, &final.coluna);
 
+    //Remover uma unidade das posições inicial e final para igualar com o index da matriz
+    inicial = removerUm(inicial);
+    final = removerUm(final);
+
     // Atualizando posição inicial
-    atual.linha = inicial.linha;
-    atual.coluna = inicial.coluna;
+    atual = inicial;
 
     // Pegando a direção
-    char caracteres[4];
+    char caracteres[5];
     scanf("%s", caracteres);
 
     return 0;

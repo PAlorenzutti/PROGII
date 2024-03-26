033f9e95ee09e41ba109aebe9ab7c21c19eb2641
Pedro Alexandre Simões Lorenzutti <pedro.lorenzutti@gmail.com>
2024-03-26 20:18:59 -0300

    Adicionado sequência de direção

--- 01_revisao/rev_05.c
+++ 01_revisao/rev_05.c
@@ -39,6 +39,46 @@ tipoPosicao removerUm(tipoPosicao posicao)
     return posicao;
 }
 
+int testarMesmaPosicao(tipoPosicao p1, tipoPosicao p2)
+{
+    if(p1.linha == p2.linha && p1.coluna == p2.coluna){
+        return 1;
+    }
+
+    return 0;
+}
+
+tipoPosicao moverPosicao(int numeroLinhas, int numeroColunas, int matriz[numeroLinhas][numeroColunas], char direcao, tipoPosicao atual)
+{
+    //aBaixo
+    if(direcao == 'B'){
+        if(atual.linha + 1 < numeroLinhas && matriz[atual.linha + 1][atual.coluna] == 0){
+            matriz[atual.linha][atual.linha] == 1;
+            atual.linha += 1;
+        }
+    //aCima
+    }else if(direcao == 'C'){
+        if(atual.linha - 1 >= 0 && matriz[atual.linha - 1][atual.coluna] == 0){
+            matriz[atual.linha][atual.linha] == 1;
+            atual.linha -= 1;       
+        }
+    //Direita
+    }else if(direcao == 'D'){
+        if(atual.coluna + 1 < numeroColunas && matriz[atual.linha][atual.coluna + 1] == 0){
+            matriz[atual.linha][atual.linha] == 1;
+            atual.coluna += 1;
+        }
+    //Esquerda
+    }else if(direcao == 'E'){
+        if(atual.coluna - 1 >= 0 && matriz[atual.linha][atual.coluna - 1] == 0){
+            matriz[atual.linha][atual.linha] == 1;
+            atual.coluna -= 1;
+        }
+    }
+
+    return atual;
+}
+
 int main() {
     int numeroLinhas, numeroColunas;
     tipoPosicao inicial, final, atual, proximo;
@@ -63,8 +103,33 @@ int main() {
     atual = inicial;
 
     // Pegando a direção
-    char caracteres[5];
-    scanf("%s", caracteres);
+    char direcoes[5];
+    scanf(" %s", direcoes);
+
+    //Printando posição inicial
+    printarPosicao(atual);
+
+    while(1){
+        for(int k = 0; k < 4; k++){
+            proximo = moverPosicao(numeroLinhas, numeroColunas, matriz, direcoes[k], atual);
+
+            //se for a mesma posição, sai do loop de teste de cada direção em sua ordem de prioridade.
+            if(!testarMesmaPosicao(atual, proximo)){
+                break;
+            }
+        }
+
+        //se o próximo for igual ao final, printar a atual, que é a útlima
+        if(testarMesmaPosicao(final, proximo)){
+            printarPosicao(atual);
+            break;
+        }else if(testarMesmaPosicao(atual, proximo)){
+            break;
+        }else{
+            atual = proximo;
+            printarPosicao(atual);
+        }  
+    }
 
     return 0;
 }

3cff93505d10dd3e1d02d5466ae6dd6204293e0e
PAlorenzutti <pedro.lorenzutti@gmail.com>
2024-03-27 12:34:26 -0300

    Adicionado bibli_02.c

---
+++ 03_bibli/bibli_02/principal.c
@@ -0,0 +1,17 @@
+#include "matrix_utils.h"
+#define MAX_ROWS 1000
+#define MAX_COLS 1000
+
+
+typedef struct{
+    int rows, cols;
+    int matrix[MAX_ROWS][MAX_COLS];
+}tMatrix;
+
+int main(){
+    tMatrix um, dois;
+
+    scanf("%d %d", &rows.um, &cols.um);
+
+    
+}

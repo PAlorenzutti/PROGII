ca34afcdd3c1aabcd91796abf63ae08fb490166d
PAlorenzutti <pedro.lorenzutti@gmail.com>
2024-06-12 13:39:28 -0300

    Adicionado rec_01.c funcionando

--- 02_recursao/rec_01/rec_01.c
+++ 02_recursao/rec_01/rec_01.c
@@ -1,40 +1,38 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 
 void imprimeInvertido(char *string){
     char stringcopy[1000];
 
+    //pegando o espaço
+
     int i;
 
-    //encontrar o espaço.
-    for(i = 0; string[i] != '\0' || string[i] != ' '; i++);
+    for(i = 0; string[i] != '\0' && string[i] != ' '; i++);
+
+    //printando ao contrário
 
     int j;
 
-    //printando a string do espaço encontado até a primeira posição
     for(j = i - 1; j >= 0; j--){
         printf("%c", string[j]);
     }
 
-    //aumentando i para não pegar o espaço antes encontrado;
+    printf(" ");
+
+    //pegando o resto da string
+
     i++;
 
-    //copiando a string
-    /*
-    for(j = 0; string[i] != '\0'; j++, i++){
-        stringcopy[j] = string[i];
-    }
+    if(string[i] != '\0'){
 
-    //fechando a copia
-    stringcopy[j] = '\0';
+        for(j = 0; string[i] != '\0'; i++, j++){
+            stringcopy[j] = string[i];
+        }
 
-    if(stringcopy[j] != '\0'){
-        printf(" ");
+        stringcopy[j] = '\0';
+
         imprimeInvertido(stringcopy);
     }
-    */
-
 }
 
 int main(){

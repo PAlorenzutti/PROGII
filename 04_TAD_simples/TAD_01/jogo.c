ea8381c43d95f375e0d167ad6de2fdc6d37a9106
Pedro Alexandre Simões Lorenzutti <pedro.lorenzutti@gmail.com>
2024-06-22 12:29:40 -0300

    Adicionado TAD_01 sem funcionar.

---
+++ 04_TAD_simples/TAD_01/jogo.c
@@ -0,0 +1,5 @@
+#include "jogo.h"
+
+tJogo CriaJogo(){
+    
+}
---
+++ 04_TAD_simples/TAD_01/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "jogo.h"
+#include "jogada.h"
+#include "jogador.h"
+#include "tabuleiro.h"
+
+int main(){
+}

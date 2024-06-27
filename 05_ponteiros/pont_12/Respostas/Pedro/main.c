b8310353c09889bbb9c50b00e243379af8dfc43d
Pedro Alexandre Simões Lorenzutti <pedro.lorenzutti@gmail.com>
2024-06-27 18:45:28 -0300

    Adicionado pont_12 90%

--- 05_ponteiros/pont_12/Respostas/Pedro/main.c
+++ 05_ponteiros/pont_12/Respostas/Pedro/main.c
@@ -12,8 +12,10 @@ int produto(int num1, int num2){
 int main(){
     Vetor vetor;
 
+    scanf("%d\n", &vetor.tamanhoUtilizado);
+
     LeVetor(&vetor);
 
-    printf("Soma: %d\n", AplicarOperacaoVetor(&vetor, soma));
+    printf("Soma: %d\n", AplicarOperacaoVetor(&vetor, soma) - 1);
     printf("Produto: %d\n", AplicarOperacaoVetor(&vetor, produto));
 }
--- 05_ponteiros/pont_12/Respostas/Pedro/vetor.c
+++ 05_ponteiros/pont_12/Respostas/Pedro/vetor.c
@@ -8,11 +8,13 @@ void LeVetor(Vetor *vetor){
 }
 
 int AplicarOperacaoVetor(Vetor *vetor, Operation op){
-    int resultado = 0;
-    
+    int resultado = 1;
+
     for(int i = 0; i < (*vetor).tamanhoUtilizado; i++){
-        resultado += op(resultado, (*vetor).elementos[i]);
+        resultado = op(resultado, (*vetor).elementos[i]);
     }
+
+    return resultado;
 }
 
 void printVetor(Vetor *vetor){

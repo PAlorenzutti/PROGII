5c613e2630c8ddc6ecb1e090720b924afcc2cdd3
Pedro Alexandre Simões Lorenzutti <pedro.lorenzutti@gmail.com>
2024-08-09 18:50:36 -0300

    Adicionado aloc_04 funcionando

---
+++ 06_alocacao_dinamica/01_geral/aloc_04/Respostas/Pedro/main.c
@@ -0,0 +1,14 @@
+#include "utils_char2.h"
+
+int main(){
+    char *vetor = CriaVetorTamPadrao();
+    int tamanho = TAM_PADRAO;
+
+    vetor = LeVetor(vetor, &tamanho);
+
+    ImprimeString(vetor);
+
+    LiberaVetor(vetor);
+
+    return 0;
+}
